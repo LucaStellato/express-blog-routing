ded8cfa1f451b98d1a80376430602122f26a423c
LucaStellato <lucastellato435@gmail.com>
2025-11-25 11:48:50 +0100

    modify route

--- routers/posts.js
+++ routers/posts.js
@@ -6,27 +6,27 @@ app.get('/posts', function (req, res) {
 })
 
 //show
-app.get('/posts/:id', function (req, res) {
+app.get('/:id', function (req, res) {
     res.send('Dettagli post')
 })
 
 //store
-app.post('/posts', function (req, res) {
+app.post('/', function (req, res) {
     res.send('creazione nuovo post')
 })
 
 //update
-app.put('/posts/:id', function (req, res) {
+app.put('/:id', function (req, res) {
     res.send('modifica dei post')
 })
 
 //modify
-app.patch('/posts/:id', function (req, res) {
+app.patch('/:id', function (req, res) {
     res.send('modifica parziale del post')
 })
 
 //destroy
-app.delete('/posts/:id', function (req, res) {
+app.delete('/:id', function (req, res) {
     res.send('eliminazione post')
 })
 module.exports = router;
